15bcbdbe271d85787a554369b819181bc92b3e67
ilyay95 <43435369+ilyay95@users.noreply.github.com>
2018-09-23 20:29:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.array = [];
+    this.compareFunction = (left, right) => left - right;
   }
 
   add(element) {
     // your implementation
+    this.array.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.array[index]
   }
 
   get length() {
     // your implementation
+    return this.array.length;
   }
 
   toArray() {
     // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    var array_sort = [];
+    indices.forEach(element => {
+      array_sort.push(this.array[element]);
+    });
+    array_sort.sort(this.compareFunction);
+    indices.sort((left, right) => left - right)
+      .forEach((element, index) => {
+        this.array[element] = array_sort[index]
+      });
+      // your implementation
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
